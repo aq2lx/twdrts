5bd9e3994f633a33cbfd93394ca3c2c1d7a763c6
Impxzior Sqerflx <impxzior@gmail.com>
2017-06-28 18:28:59 +0700

    Updated: layouts and function

--- public/js/scav.js
+++ public/js/scav.js
@@ -1,20 +1,94 @@
 'use strict';
 
+var name = '';
+var star = 5;
+var tier = 4;
+var lvl = 1;
+var xp = 0;
 var countMember = 2;
 
 // Elements
+var eName = document.getElementById('ipt-name');
+
+var eLblLvl = document.getElementById('lbl-lvl');
+var eInputLvl = document.getElementById('range-lvl');
+
+var eGroupStar = document.getElementById('g-star');
+var eGroupTier = document.getElementById('g-tier');
+var eBtnStar = eGroupStar.getElementsByTagName('button');
+var eBtnTier = eGroupTier.getElementsByTagName('button');
+
 var btnAddtoCamp = document.getElementById('btn-addtocamp');
 var camp = document.getElementById('camp');
 var campNodes = camp.getElementsByTagName('li');
 
+var eXP = document.getElementById('ipt-xp');
+
+// Input Change
+eName.onchange = function () {
+  name = this.value;
+};
+
+// Input XP
+eXP.onclick = function () {
+  this.select();
+};
+
+eXP.onkeyup = function () {
+  xp = parseInt(this.value, 10);
+};
+
+// Input Level
+eInputLvl.oninput = function () {
+  eLblLvl.innerHTML = lvl = parseInt(this.value);
+};
+
+// Input Star
+for (var i = 0; i < eBtnStar.length; i++) {
+  eBtnStar[i].onclick = function () {
+    for (var j = 0; j < eBtnStar.length; j++) {
+      eBtnStar[j].classList.remove('active');
+    }
+
+    this.classList.add('active');
+    star = parseInt(this.dataset.value, 10);
+
+    setMaxLevel();
+  };
+}
+
+// Input Tier
+for (var _i = 0; _i < eBtnTier.length; _i++) {
+  eBtnTier[_i].onclick = function () {
+    for (var j = 0; j < eBtnTier.length; j++) {
+      eBtnTier[j].classList.remove('active');
+    }
+
+    this.classList.add('active');
+    tier = parseInt(this.dataset.value, 10);
+
+    setMaxLevel();
+  };
+}
+
+// Set max level
+var setMaxLevel = function setMaxLevel() {
+  var maxLvl = MaxLevel['s' + star]['t' + tier];
+
+  eInputLvl.setAttribute('max', maxLvl);
+  eInputLvl.value = eLblLvl.innerHTML = lvl = 1;
+};
+
 // Add to camp
 var addToCamp = function addToCamp(data) {
+  console.log(data);
+
   var idx = void 0;
 
   // Check empty nodes
-  for (var i = 0; i < 5; i++) {
-    if (!campNodes[i].hasChildNodes()) {
-      idx = i;
+  for (var _i2 = 0; _i2 < 5; _i2++) {
+    if (!campNodes[_i2].hasChildNodes()) {
+      idx = _i2;
 
       break;
     }
@@ -25,6 +99,7 @@ var addToCamp = function addToCamp(data) {
 
   // Append child
   newMember.appendChild(addBtnRemove(idx));
+  newMember.appendChild(addTier(data.tier));
 
   campNodes[idx].appendChild(newMember);
 
@@ -45,6 +120,22 @@ var addBtnRemove = function addBtnRemove(idx) {
   return btnRemove;
 };
 
+var addTier = function addTier(tier) {
+  var wrapper = document.createElement('div');
+
+  wrapper.className = 'tier-wrapper';
+
+  for (var _i3 = 0; _i3 < tier; _i3++) {
+    var _tier = document.createElement('span');
+
+    _tier.className = 'tier';
+
+    wrapper.appendChild(_tier);
+  }
+
+  return wrapper;
+};
+
 // Remove member
 var removeMember = function removeMember(evt) {
   var node = campNodes[evt.target.idx];
@@ -62,5 +153,11 @@ btnAddtoCamp.onclick = function () {
     return false;
   }
 
-  addToCamp();
+  addToCamp({
+    name: name,
+    star: star,
+    tier: tier,
+    lvl: lvl,
+    xp: xp
+  });
 };
--- src/js/scav.js
+++ src/js/scav.js
@@ -1,12 +1,86 @@
+let name = ''
+let star = 5
+let tier = 4
+let lvl = 1
+let xp = 0
 let countMember = 2
 
 // Elements
+const eName = document.getElementById('ipt-name')
+
+const eLblLvl = document.getElementById('lbl-lvl')
+const eInputLvl = document.getElementById('range-lvl')
+
+const eGroupStar = document.getElementById('g-star')
+const eGroupTier = document.getElementById('g-tier')
+const eBtnStar = eGroupStar.getElementsByTagName('button')
+const eBtnTier = eGroupTier.getElementsByTagName('button')
+
 const btnAddtoCamp = document.getElementById('btn-addtocamp')
 const camp = document.getElementById('camp')
 const campNodes = camp.getElementsByTagName('li')
 
+const eXP = document.getElementById('ipt-xp')
+
+// Input Change
+eName.onchange = function() {
+  name = this.value
+}
+
+// Input XP
+eXP.onclick = function() {
+  this.select()
+}
+
+eXP.onkeyup = function() {
+  xp = parseInt(this.value, 10)
+}
+
+// Input Level
+eInputLvl.oninput = function () {
+  eLblLvl.innerHTML = lvl = parseInt(this.value)
+}
+
+// Input Star
+for (let i = 0; i < eBtnStar.length; i++) {
+  eBtnStar[i].onclick = function() {
+    for (let j = 0; j < eBtnStar.length; j++) {
+      eBtnStar[j].classList.remove('active')
+    }
+
+    this.classList.add('active')
+    star = parseInt(this.dataset.value, 10)
+
+    setMaxLevel()
+  }
+}
+
+// Input Tier
+for (let i = 0; i < eBtnTier.length; i++) { 
+  eBtnTier[i].onclick = function() {
+    for (let j = 0; j < eBtnTier.length; j++) {
+      eBtnTier[j].classList.remove('active')
+    }
+
+    this.classList.add('active')
+    tier = parseInt(this.dataset.value, 10)
+
+    setMaxLevel()
+  }
+}
+
+// Set max level
+const setMaxLevel = function () {
+  const maxLvl = MaxLevel[`s${star}`][`t${tier}`]
+
+  eInputLvl.setAttribute('max', maxLvl)
+  eInputLvl.value = eLblLvl.innerHTML = lvl = 1
+}
+
 // Add to camp
 const addToCamp = function (data) {
+  console.log(data)
+
   let idx
 
   // Check empty nodes
@@ -23,7 +97,8 @@ const addToCamp = function (data) {
 
   // Append child
   newMember.appendChild(addBtnRemove(idx))
-  
+  newMember.appendChild(addTier(data.tier))
+
   campNodes[idx].appendChild(newMember)
 
   countMember++
@@ -43,6 +118,22 @@ const addBtnRemove = function (idx) {
   return btnRemove;
 }
 
+const addTier = function (tier) {
+  const wrapper = document.createElement('div')
+
+  wrapper.className = 'tier-wrapper'
+
+  for (let i = 0; i < tier; i++) {
+    const tier = document.createElement('span')
+
+    tier.className = 'tier'
+
+    wrapper.appendChild(tier)
+  }
+
+  return wrapper
+}
+
 // Remove member
 const removeMember = function (evt) {
   const node = campNodes[evt.target.idx]
@@ -60,5 +151,11 @@ btnAddtoCamp.onclick = function() {
     return false
   }
 
-  addToCamp()
+  addToCamp({
+    name: name,
+    star: star,
+    tier: tier,
+    lvl: lvl,
+    xp: xp
+  })
 }
