f52a6de73206d43e5f2723bae1129f90abcb514d
Impxzior Sqerflx <impxzior@gmail.com>
2018-03-15 02:06:46 +0700

    refactor code

--- public/js/scav.js
+++ public/js/scav.js
@@ -1,35 +1,74 @@
 'use strict';
 
+var _db = require('./db.json');
+
 var Scav = {
 
   config: {
     xpFromScav: 100000
   },
 
+  input: {
+    star: 6
+  },
+
   init: function init(options) {
-    this.defineEl();
-    this.iniCamp();
+    this.defineEl().iniCamp().iniModal();
   },
   defineEl: function defineEl() {
     this.el = {
-      modalAddMember: document.getElementById('modal-addmember')
+      modalAddMember: document.getElementById('modal-addmember'),
+      btnCloseModal: document.getElementById('btn-close-md')
     };
+
+    return this;
   },
   iniCamp: function iniCamp() {
     var _this = this;
 
     var camp = document.getElementById('camp');
-    var campNodes = camp.getElementsByTagName('li');
+    var campNodes = this.el.campNodes = camp.getElementsByTagName('li');
 
-    Array.prototype.forEach.call(campNodes, function (el) {
+    Array.prototype.forEach.call(this.el.campNodes, function (el) {
       el.onclick = function () {
-        this.toggleModal(this.el.modalAddMember);
-      }.bind(_this);
+        _this.toggleModal(_this.el.modalAddMember);
+      };
     });
+
+    return this;
+  },
+  iniModal: function iniModal() {
+    var _this2 = this;
+
+    var eGroupStar = document.getElementById('g-star');
+    var eGroupTier = document.getElementById('g-tier');
+    var eBtnStar = eGroupStar.getElementsByTagName('button');
+    var eBtnTier = eGroupTier.getElementsByTagName('button');
+
+    for (var i = 0; i < eBtnStar.length; i++) {
+      eBtnStar[i].onclick = function (e) {
+        for (var j = 0; j < eBtnStar.length; j++) {
+          eBtnStar[j].classList.remove('active');
+        }
+
+        var el = e.target || e.currentTarget;
+        el.classList.add('active');
+
+        _this2.input.star = parseInt(el.dataset.value, 10);
+        _this2.setMaxLevel();
+      };
+    }
+
+    this.el.btnCloseModal.onclick = function () {
+      _this2.toggleModal(_this2.el.modalAddMember);
+    };
+
+    return this;
   },
   toggleModal: function toggleModal(modal) {
-    modal.classList.toggle('show');
-  }
+    modal.classList.toggle('md-show');
+  },
+  setMaxLevel: function setMaxLevel() {}
 };
 
 window.Scav = Scav;
@@ -37,3 +76,5 @@ window.Scav = Scav;
 // ----->
 
 Scav.init();
+
+console.log(_db.db);
--- src/js/scav.js
+++ src/js/scav.js
@@ -1,33 +1,76 @@
+import { db } from './db.json'
+
 const Scav = {
 
   config: {
-    xpFromScav: 100000
+    xpFromScav: 100000,
+  },
+
+  input: {
+    star: 6
   },
 
   init(options) {
     this.defineEl()
-    this.iniCamp()
+        .iniCamp()
+        .iniModal()
   },
 
   defineEl() {
     this.el = {
-      modalAddMember: document.getElementById('modal-addmember')
+      modalAddMember: document.getElementById('modal-addmember'),
+      btnCloseModal: document.getElementById('btn-close-md')
     }
+
+    return this
   },
 
   iniCamp() {
     const camp = document.getElementById('camp')
-    const campNodes = camp.getElementsByTagName('li')
+    const campNodes = this.el.campNodes = camp.getElementsByTagName('li')
 
-    Array.prototype.forEach.call(campNodes, el => {
-      el.onclick = (function() {
+    Array.prototype.forEach.call(this.el.campNodes, el => {
+      el.onclick = (() => {
         this.toggleModal(this.el.modalAddMember)
-      }).bind(this)
+      })
     })
+
+    return this
+  },
+
+  iniModal() {
+    const eGroupStar = document.getElementById('g-star')
+    const eGroupTier = document.getElementById('g-tier')
+    const eBtnStar = eGroupStar.getElementsByTagName('button')
+    const eBtnTier = eGroupTier.getElementsByTagName('button')
+
+    for (let i = 0; i < eBtnStar.length; i++) {
+      eBtnStar[i].onclick = (e) => {
+        for (let j = 0; j < eBtnStar.length; j++) {
+          eBtnStar[j].classList.remove('active')
+        }
+
+        const el = e.target || e.currentTarget
+        el.classList.add('active')
+
+        this.input.star = parseInt(el.dataset.value, 10)
+        this.setMaxLevel()
+      }
+    }
+
+    this.el.btnCloseModal.onclick = (() => {
+      this.toggleModal(this.el.modalAddMember)
+    })
+
+    return this
   },
 
   toggleModal(modal) {
-    modal.classList.toggle('show')
+    modal.classList.toggle('md-show')
+  },
+
+  setMaxLevel() {
+
   }
 }
 
@@ -36,3 +79,5 @@ window.Scav = Scav
 // ----->
 
 Scav.init()
+
+console.log(db)
