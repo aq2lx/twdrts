5263e0b667803588b9e48d2bfa947777c2bdcb5f
Impxzior Sqerflx <impxzior@gmail.com>
2018-06-05 21:28:58 +0700

    ar - fixed ap 8% from yellow weapons

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const sass = require('gulp-sass')
+const uglify = require('gulp-uglify')
+const cleanCSS = require('gulp-clean-css')
+const rename = require('gulp-rename')
+
+gulp.task('default', ['ar', 'scav', 'css', 'watch'])
+
+// Script
+gulp.task('ar', () => {
+  return gulp.src('src/js/ar.js')
+             .pipe(babel({
+               presets: ['es2015']
+             }))
+             .pipe(gulp.dest('public/js'))
+})
+
+gulp.task('scav', () => {
+  return gulp.src('src/js/scav.js')
+             .pipe(babel({
+               presets: ['es2015']
+             }))
+             .pipe(gulp.dest('public/js'))
+})
+
+// CSS
+gulp.task('css', () => {
+  return gulp.src('src/sass/app.scss')
+             .pipe(sass())
+             .pipe(gulp.dest('public/css'))
+})
+
+// Min
+gulp.task('uglify', () => {
+  return gulp.src(['src/js/ar.js', 'src/js/scav.js'])
+             .pipe(babel({
+               presets: ['es2015']
+             }))
+             .pipe(uglify())
+             .pipe(gulp.dest('public/js'))
+})
+
+gulp.task('minify-css', () => {
+  return gulp.src('src/sass/app.scss')
+             .pipe(sass())
+             .pipe(cleanCSS())
+             .pipe(gulp.dest('public/css'));
+})
+
+gulp.task('prod', ['uglify', 'minify-css'])
+
+// Watch
+gulp.task('watch', () => {
+  gulp.watch('src/js/ar.js', ['ar'])
+  gulp.watch('src/js/scav.js', ['scav'])
+  gulp.watch('src/sass/**/*.scss', ['css'])
+})
